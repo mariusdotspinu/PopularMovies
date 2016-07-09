e5e76da0e153332db4a3bcfb6736a86021ce934a
Marius Spinu <mariusdotspinu@gmail.com>
2016-07-09 15:17:28 +0300

    Update FetchMovieReviews.java

--- PopularMovies/app/src/main/java/app/com/example/marius/popularmovies/FetcherTasks/FetchMovieReviews.java
+++ PopularMovies/app/src/main/java/app/com/example/marius/popularmovies/FetcherTasks/FetchMovieReviews.java
@@ -50,7 +50,7 @@ public class FetchMovieReviews extends AsyncTask<Long, Void, ArrayList<ArrayList
 
         try {
             final String BASE_URL = "http://api.themoviedb.org/3/movie/" + params[0] +
-                    "/reviews?api_key=88ac9738e1138283396eff8b210de62b";
+                    "/reviews?api_key=[API_KEY]";
 
             Uri builtUri = Uri.parse(BASE_URL).buildUpon().build();
 
